6528bceb2acb770786958384ec0e17b74fef3135
CerealAxis <2741798712@qq.com>
2024-01-13 20:44:27 +0800

    实现植物的摇摆

--- 植物大战僵尸/main.cpp
+++ 植物大战僵尸/main.cpp
@@ -15,6 +15,13 @@ IMAGE* imgZhiWu[ZHI_WU_COUNT][20];
 int curX, curY;//当前选中植物运动过程中的x,y坐标
 int curZhiWu;//0:无选择 1:第一种
 
+struct zhiwu {
+	int type;//0：没有植物 1：第一种植物
+	int frameIndex;//序列帧序号
+};
+
+struct zhiwu map[3][9];
+
 bool fileExist(const char* name) {
 	FILE* fp = fopen(name, "r");
 	if (fp == NULL) {
@@ -32,6 +39,7 @@ void gameInit() {
 	loadimage(&imgBar, "res/res/bar5.png");
 
 	memset(imgZhiWu, 0, sizeof(imgZhiWu));
+	memset(map, 0, sizeof(map));
 
 	//初始化植物卡牌
 	char name[64];
@@ -40,7 +48,7 @@ void gameInit() {
 		loadimage(&imgCards[i], name);
 
 		for (int j = 0; j < 20; j++) {
-			sprintf_s(name, sizeof(name), "res/res/zhiwu/%d/%d.png", i ,j+1);
+			sprintf_s(name, sizeof(name), "res/res/zhiwu/%d/%d.png", i, j + 1);
 			if (fileExist(name)) {
 				imgZhiWu[i][j] = new IMAGE;
 				loadimage(imgZhiWu[i][j], name);
@@ -69,9 +77,22 @@ void updateWindow() {
 	}
 
 	//渲染拖动过程
-	if (curZhiWu>0) {
+	if (curZhiWu > 0) {
 		IMAGE* img = imgZhiWu[curZhiWu - 1][0];
-		putimagePNG(curX-img->getwidth()/2.0, curY-img->getheight()/2.0, img);
+		putimagePNG(curX - img->getwidth() / 2.0, curY - img->getheight() / 2.0, img);
+	}
+
+	//渲染种植的植物
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (map[i][j].type > 0) {
+				int x = 256 + j * 81;
+				int y = 179 + i * 102 + 14;
+				int zhiwuType = map[i][j].type - 1;
+				int index = map[i][j].frameIndex;
+				putimagePNG(x, y, imgZhiWu[zhiwuType][index]);
+			}
+		}
 	}
 
 	EndBatchDraw();
@@ -93,7 +114,32 @@ void userClick() {
 			curY = msg.y;
 		}
 		else if (msg.message == WM_LBUTTONUP) {
+			if (msg.x > 256 && msg.y > 179 && msg.y < 489) {
+				int row = (msg.y - 179) / 102;
+				int col = (msg.x - 256) / 81;
 
+				if (map[row][col].type == 0) {
+					map[row][col].type = curZhiWu;
+					map[row][col].frameIndex = 0;
+				}
+			}
+			curZhiWu = 0;
+			status = 0;
+		}
+	}
+}
+
+void updateGame() {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (map[i][j].type > 0) {
+				map[i][j].frameIndex++;
+				int zhiwuType = map[i][j].type - 1;
+				int index = map[i][j].frameIndex;
+				if (imgZhiWu[zhiwuType][index] == NULL) {
+					map[i][j].frameIndex = 0;
+				}
+			}
 		}
 	}
 }
@@ -103,6 +149,8 @@ int main(void) {
 	while (1) {
 		userClick();
 		updateWindow();
+		updateGame();
+		Sleep(2);
 	}
 	system("pause");
 	return 0;
